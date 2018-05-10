feb87b2418bdc0d7fcf58535a71b7168e5e6157b
Michael Kamau <mikekamau@Michaels-MacBook-Pro-2.local>
2018-05-10 11:46:12 +0300

    Initial project setup

---
+++ app/src/main/java/mikekamau/com/openchat/MainActivity.java
@@ -0,0 +1,13 @@
+package mikekamau.com.openchat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
