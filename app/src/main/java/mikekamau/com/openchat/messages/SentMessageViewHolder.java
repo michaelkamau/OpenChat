ee39557ec4a32bcc2ba7b24143aaea520103cba7
Michael Kamau <tokamau@gmail.com>
2018-05-15 10:36:34 +0300

    Add binding method for views in SentMessage holder

--- app/src/main/java/mikekamau/com/openchat/messages/SentMessageViewHolder.java
+++ app/src/main/java/mikekamau/com/openchat/messages/SentMessageViewHolder.java
@@ -1,12 +1,16 @@
 package mikekamau.com.openchat.messages;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import mikekamau.com.openchat.R;
+import mikekamau.com.openchat.entities.ChatMessage;
 
 public class SentMessageViewHolder extends RecyclerView.ViewHolder {
+
+    private static final String TAG = "SentMessageViewHolder";
 
     private TextView sentMessageTextView;
     private TextView sentTimeTextView;
@@ -15,5 +19,17 @@ public class SentMessageViewHolder extends RecyclerView.ViewHolder {
         super(itemView);
         sentMessageTextView = itemView.findViewById(R.id.tv_sent_message);
         sentTimeTextView = itemView.findViewById(R.id.tv_sent_time);
+    }
+
+    public void bind(final ChatMessage chatMessage) {
+        String message = chatMessage.getMessage();
+        if (message != null && !message.isEmpty()) {
+            sentTimeTextView.setText(message);
+            String timeSent = chatMessage.getTimestamp();
+            //TODO: Create a TimeUtils class for time conversions
+            sentTimeTextView.setText(timeSent);
+        } else {
+            Log.d(TAG, "Invalid message");
+        }
     }
 }
